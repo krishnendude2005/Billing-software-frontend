c924cbf3e28f4326198a8873b7b087fe2eec5f41
Krishnendu De <krishnendu1205.be22@chitkarauniversity.edu.in>
2026-02-03 19:58:35 +0530

    Implemented the fucntionality to add a new category

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,5 +1,7 @@
 import logo from './logo.png'
+import upload from './File_Upload_Image.png'
 
 export const assets = {
-    logo
+    logo,
+    upload
 }
--- src/components/CategoryForm/CtaegoryForm.jsx
+++ src/components/CategoryForm/CtaegoryForm.jsx
@@ -1,21 +1,99 @@
+import { useContext, useEffect, useState } from "react";
+import { assets } from "../../assets/assets";
+import toast from "react-hot-toast";
+import { addCategory } from "../../service/CategoryService";
+import { AppContext } from "../../context/appContext";
+
 const CategoryForm = () => {
+    const { categories, setCategories } = useContext(AppContext);
+
+    const [loading, setLoading] = useState(false);
+    const [image, setImage] = useState(false);
+    const [data, setData] = useState({
+        name: "",
+        description: "",
+        bgColor: "#2c2c2c"
+    });
+
+    useEffect(() => {
+        console.log(data);
+
+    }, [data]);
+
+    const onChangeHandler = (e) => {
+        const { name, value } = e.target;
+        setData({
+            ...data,
+            [name]: value
+        });
+    }
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+        // handle form submission logic here
+        setLoading(true);
+        if (!image) {
+            toast.error("Please upload an image for the category");
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append("category", JSON.stringify(data));
+        formData.append("file", image);
+
+        try {
+            const response = await addCategory(formData)
+            if (response.status === 201) {
+                setCategories([...categories, response.data])
+                toast.success("Category added successfully");
+                setData({
+                    name: "",
+                    description: "",
+                    bgColor: "#2c2c2c"
+                });
+                setImage(false);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error("Error adding category");
+
+        } finally {
+            setLoading(false);
+        }
+    }
+
     return (
         <div className="mx-2 mt-2">
             <div className="row">
-                <div className="card col-md-8 form-container">
+                <div className="card col-md-12 form-container">
                     <div className="card-body">
-                        <form>
+                        <form onSubmit={onSubmitHandler}>
                             <div className="mb-3">
+                                
                                 <label htmlFor="image" className="form-label">
-                                    <img src="https://placehold.co/48x48" alt="" width={48} />
+                                    <img src={image ? URL.createObjectURL(image) : assets.upload} alt="" width={48} />
+                                    
                                 </label>
-
-                                <input type="file" name="image" id="image" className="form-control" hidden />
+                                <input
+                                    type="file"
+                                    name="image"
+                                    id="image"
+                                    className="form-control"
+                                    hidden
+                                    onChange={(e) => setImage(e.target.files[0])}
+                                />
                             </div>
 
                             <div className="mb-3">
                                 <label htmlFor="name" className="form-label">Name</label>
-                                <input type="text" name="name" id="name" className="form-control" placeholder="Category Name" />
+                                <input type="text"
+                                    name="name"
+                                    id="name"
+                                    className="form-control"
+                                    placeholder="Category Name"
+                                    onChange={onChangeHandler}
+                                    value={data.name}
+                                />
                             </div>
 
                             <div className="mb-3">
@@ -25,8 +103,10 @@ const CategoryForm = () => {
                                     name="description"
                                     id="description"
                                     className="form-control"
-                                    placeholder="Category Description">
-
+                                    placeholder="Category Description"
+                                    onChange={onChangeHandler}
+                                    value={data.description}
+                                >
                                 </textarea>
                             </div>
 
@@ -36,10 +116,16 @@ const CategoryForm = () => {
                                 <input type="color"
                                     name="bgColor"
                                     id="bgcolor"
-                                    placeholder="#ffffff" />
+                                    placeholder="#ffffff"
+                                    onChange={onChangeHandler}
+                                    value={data.bgColor}
+                                />
                             </div>
-                            <button type="submit" className="btn btn-warning w-100">
-                                Save
+                            <button 
+                            type="submit"
+                            disabled={loading}
+                             className="btn btn-warning w-100">
+                                {loading ? "Adding Category..." : "Add Category"}
                             </button>
                         </form>
                     </div>
