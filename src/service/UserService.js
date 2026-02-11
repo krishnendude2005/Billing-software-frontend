8f6cf895038a71bd5c1d9c015e3de1a746b52415
Krishnendu De <krishnendu1205.be22@chitkarauniversity.edu.in>
2026-02-11 09:53:32 +0530

    Added the endpoint calling service for items and users

---
+++ src/service/UserService.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+
+export const addUser = async (userData) => {
+    return await axios.post("http://localhost:8080/api/v1.0/admin/register", userData, {headers: {'Authorization': `Bearer ${localStorage.getItem("token")}`}});
+}
+
+export const deleteUser = async (userId) => {
+    return await axios.delete(`http://localhost:8080/api/v1.0/admin/users/${userId}`, {headers: {'Authorization': `Bearer ${localStorage.getItem("token")}`}});
+}
+
+export const fetchUsers = async () => {
+    return await axios.get("http://localhost:8080/api/v1.0/admin/users", {headers: {'Authorization': `Bearer ${localStorage.getItem("token")}`}});
+}
