4af5922146509e5b6fd32a89f9c13b6df88676cc
Krishnendu De <krishnendu1205.be22@chitkarauniversity.edu.in>
2026-02-12 23:40:24 +0530

    Implemented the category in the explore page

---
+++ src/components/Category/Catergory.jsx
@@ -0,0 +1,24 @@
+import './Category.css'
+
+const Category = ({ categoryName, imageUrl, numberOfItems, bgColor, isSelected, onClick }) => {
+       console.log("IMAGE URL:", imageUrl);
+
+    return (
+        <div className="d-flex align-items-center p-3 rounded gap-1 position-relative category-hover"
+            style={{ backgroundColor: bgColor, cursor: 'pointer' }}
+            onClick={onClick}
+        >
+
+            <div style={{ position: 'relative', marginRight: '15px' }}>
+                <img src={imageUrl} alt={categoryName} className="category-image" />
+            </div>
+            <div>
+                <h6 className='text-white mb-0'>{categoryName}</h6>
+                <p className='text-white mb-0'>{numberOfItems} Items</p>
+            </div>
+            {isSelected && <div className='active-category'></div>}
+        </div>
+    )
+}
+
+export default Category;
