c5420df9f9155c3bf501170f260f0e99daab2c5e
Krishnendu De <krishnendu1205.be22@chitkarauniversity.edu.in>
2026-02-19 22:25:38 +0530

    Implemented All Items Card to display all category items together

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,9 +2,11 @@ import logo from './logo.png'
 import upload from './Upload_File.png'
 import login from './login-bg.jpg'
 import profile from './Profile-logo.png'
+import AllItems from './All_Items.png'
 export const assets = {
     logo,
     upload,
     login,
-    profile
+    profile,
+    AllItems
 }
