2286ebde1e46c0bcc8d81e234e93f35e2c35fd2d
Krishnendu De <krishnendu1205.be22@chitkarauniversity.edu.in>
2026-03-04 17:03:18 +0530

    Changed the backend URL

--- src/service/AuthService.js
+++ src/service/AuthService.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-
+ const renderUrl = "billingsoftware-latest-46yn.onrender.com"
+ const railwayUrl = "billingsoftwarebackend-production.up.railway.app"
+ 
 export const login = async(data) => {
-   return await axios.post("https://billingsoftwarebackend-production.up.railway.app/api/v1.0/login", data)
+   return await axios.post("https://billingsoftware-latest-46yn.onrender.com/api/v1.0/login", data)
 }
